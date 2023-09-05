a251908ea50fe5771dd1f9cca8b31dd130a127e1
Mohammad Karimi <mohammadk0094.mk@gmail.com>
2023-09-05 17:32:08 +0330

    Added usage message

--- main.cc
+++ main.cc
@@ -5,6 +5,14 @@
 
 int main(int argc, char const *argv[])
 {
+    if (argc != 2){
+        std::cout << "Usage: " << argv[0] << " ARG" << "\nAvailable ARGS:"
+        "\n[1]: ts (Run tcp server)"
+        "\n[2]: us (Run udp server)"
+        "\n[3]: tc (Connect to tcp server)"
+        "\n[4]: uc (Connect to udp server)\n";
+        return -1;
+        }
     if (strncmp(argv[1], "ts",2)==0){
         std::cout << "Running TCP Server\n";
         createTCPServer(8080);
